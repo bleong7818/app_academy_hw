1847cb6f430aafa62887fdda2cebfc79c12aac14
Brandon Leong <bjtl1105@gmail.com>
2020-08-14 08:39:36 -0700

    Added Queue Class

--- W3D5 HW/W3D5_HW.rb
+++ W3D5 HW/W3D5_HW.rb
@@ -18,6 +18,25 @@ class Stack
     end
   end
 
+class Queue
+
+  def initialize(queue)
+    @queue = queue
+  end
+
+  def enqueue(el)
+    @queue << el
+  end
 
 
+  def dequeue
+    @queue.shift
+  end
+
+  def peek
+    p @queue[-1]
+  end
+
+
+end
 
