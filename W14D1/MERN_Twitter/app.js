11a30c3bbaa414bcbe1c87273b1758e3c87f573a
Brandon Leong <bjtl1105@gmail.com>
2020-10-24 15:10:44 -0700

    Server up and running

--- W14D1/MERN_Twitter/app.js
+++ W14D1/MERN_Twitter/app.js
@@ -2,8 +2,15 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => res.send("Hello"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
