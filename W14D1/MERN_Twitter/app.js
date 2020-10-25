8ac2aacfdfccbdfb7836d958daff82a13de81f2e
Brandon Leong <bjtl1105@gmail.com>
2020-10-25 11:04:32 -0700

    first commit

--- W14D1/MERN_Twitter/app.js
+++ W14D1/MERN_Twitter/app.js
@@ -4,9 +4,32 @@ const app = express();
 const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
+const User = require('./models/User')
 const bodyParser = require('body-parser');
 
-app.get("/", (req, res) => res.send("Hello"));
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
+
+app.get("/", (req, res) => { 
+    const user = new User({
+        handle: "Josh",
+        email: "JYBright@liberl.orb",
+        password: "EyesOfAmber"
+    })
+    user.save()
+    res.send("Hello");
+});
+
+
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,8 +38,3 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
-
-mongoose
-    .connect(db, { useNewUrlParser: true })
-    .then(() => console.log("Connected to MongoDB successfully"))
-    .catch(err => console.log(err));
